fbc81c2373865403dada3e655c487fe7ff2f4dda
Vitoria Magalhães <165837941+VitoriaMagalhaess@users.noreply.github.com>
2025-03-05 00:59:57 +0000

    questão 4 resolvida

---
+++ Questões-Resolvidas/Questão4.py
@@ -0,0 +1,15 @@
+def calcular_percentual(faturamento_estado,faturamento_total):
+    return(faturamento_estado /faturamento_total)*100
+
+    distribuidora={
+        "SP": 67836.43,
+        "RJ": 36678.66,
+        "MG": 29229.88,
+        "ES": 27165.48,
+        "Outros": 19849.53
+    }
+
+faturamento_total=sum(distribuidora.values())
+for estado, valor in distribuidora.items():
+    percentual=calcular_percentual(valor,faturamento_total)
+    print(f"{estado}: {percentual:2f}%")
